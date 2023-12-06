a9163c529a56dc8bd38f0a8947f9942913b7d2c6
Katehok <a.timin2003@mail.ru>
2023-12-06 15:32:29 +0300

    Added parallel pragma

--- src/shell_sort.c
+++ src/shell_sort.c
@@ -4,18 +4,20 @@
 
 double shell_sort(unsigned int* arr, int count, int threads)
 {
+	int k;
 	double start, end;
 	start = omp_get_wtime();
 
 	for (int d = count / 2; d > 0; d /= 2) {
-		for (int i = d; i < count; i += d) {
-			for (int j = i - d; j >= 0; j -= d) {
-				if (arr[j] <= arr[j + d]) break;
-				unsigned int tmp = arr[j];
-				arr[j] = arr[j + d];
-				arr[j + d] = tmp;
-			}
-		}
+		#pragma omp parallel for num_threads(threads), private(k)
+		for (k = 0; k < d; ++k)						// каждый поток меняет только свои данные
+			for (int i = d + k; i < count; i += d)	// сортировка вставками
+				for (int j = i - d; j >= 0; j -= d) {
+					if (arr[j] <= arr[j + d]) break;
+					unsigned int tmp = arr[j];
+					arr[j] = arr[j + d];
+					arr[j + d] = tmp;
+				}
 	}
 
 	end = omp_get_wtime();
